d40adcd011309ec4c6970733fa66c9e0819a4753
Zero0Q <elvissalihovic.es@gmail.com>
2025-01-29 12:37:30 +0800

    Update addon.js

--- addon.js
+++ addon.js
@@ -25,7 +25,7 @@ const manifest = {
             extra: [
                 {
                     name: 'genre',
-                    options: ['Action','Adventure','Animation','Biography','Comedy','Crime','Documentary','Drama','Family','Fantasy','Film Noir','History','Horror','Music','Musical','Mystery','Romance','Sci-Fi','S...],
+                    options: ['Action','Adventure','Animation','Biography','Comedy','Crime','Documentary','Drama','Family','Fantasy','Film Noir','History','Horror','Music','Musical','Mystery','Romance','Sci-Fi','Short Film','Sport','Superhero','Thriller','War','Western'],
                     isRequired: false
                 }
             ]
